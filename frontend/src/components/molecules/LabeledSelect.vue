03a6a8a10862814af08377d5d7e303044316d7a7
Pavel Zhuravlyov <zd.kane@hotmail.com>
2021-07-04 02:46:30 +0300

    added csv export;

--- frontend/src/components/molecules/LabeledSelect.vue
+++ frontend/src/components/molecules/LabeledSelect.vue
@@ -1,39 +1,76 @@
 <template>
-  <div class="wrapper">
-    <p>{{ header }}</p>
+  <div>
+    <div class="header">
+      <p>{{ header }}</p>
+      <button v-if="selected" @click="chooseDomain(null)">Clear</button>
+    </div>
+
     <div>
-      <div
+      <button
         class="option"
         v-for="option in options"
         :key="option.value"
         :value="option.label"
-        @click="close = 'X'"
+        @click="chooseDomain(option.value)"
       >
         {{ option.label }} {{ close }}
-      </div>
+      </button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { Options, Vue } from "vue-class-component";
 import { Prop } from "vue-property-decorator";
 
 export interface OptionType {
-  value: any;
+  value: string;
   label: string;
 }
 
+@Options({
+  emits: ["domain"],
+})
 export default class LabeledSelect extends Vue {
   @Prop({ required: true }) readonly options!: OptionType;
   @Prop() readonly header: string = "";
 
   private close = "";
+  private selected: string | null = "";
+
+  public chooseDomain(opVal: string | null): void {
+    this.selected = opVal;
+    this.$emit("domain", opVal);
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.wrapper {
+.header {
+  display: flex;
+  align-items: center;
+
+  & button {
+    margin-left: 40px;
+    background-color: white;
+    border: solid 1px;
+    border-radius: 20px;
+    height: 18px;
+    text-align: center;
+
+    &:focus,
+    &:hover {
+      cursor: pointer;
+    }
+
+    &:active {
+      background-color: rgb(131, 64, 64);
+    }
+
+    &:hover {
+      background-color: rgb(238, 238, 238);
+    }
+  }
 }
 .option {
   &:hover {
--- frontend/src/components/templates/Table.vue
+++ frontend/src/components/templates/Table.vue
@@ -5,7 +5,17 @@
         <table>
           <thead>
             <tr>
-              <th>ID</th>
+              <th>
+                <input
+                  type="checkbox"
+                  v-model="allCheck"
+                  @change="registerEmailId($event)"
+                />
+              </th>
+              <th @click="sortIDs">
+                ID
+                {{ params.orderByColumn === "id" ? sortSymbol : null }}
+              </th>
               <th @click="sortEmails">
                 E-mail
                 {{ params.orderByColumn === "email" ? sortSymbol : null }}
@@ -19,10 +29,17 @@
           </thead>
           <tbody>
             <tr v-for="record in emailRecords" :key="record.id">
+              <th>
+                <input
+                  type="checkbox"
+                  :checked="record.checked"
+                  @change="registerEmailId($event, record.id)"
+                />
+              </th>
               <th>{{ record.id }}</th>
               <th>{{ record.email }}</th>
               <th>{{ record.created_at }}</th>
-              <th class="del-label"></th>
+              <th @click="deleteRecord(record.id)" class="del-label"></th>
             </tr>
           </tbody>
         </table>
@@ -37,6 +54,9 @@
             />
             <button @click="applyPagination">OK</button>
           </div>
+          <div v-if="checkedEmails.length">
+            <button @click="exportEmails">Export e-mails</button>
+          </div>
           <div>
             <button @click="prev">&lt;</button>
             <button @click="next">&gt;</button>
@@ -47,6 +67,7 @@
         class="domain-select"
         :header="'Domains'"
         :options="domains"
+        @domain="filterByDomains"
       />
     </div>
   </div>
@@ -58,12 +79,21 @@ import { serverURL } from "@/constants";
 import { OptionType } from "@/components/molecules/LabeledSelect.vue";
 import LabeledSelect from "@/components/molecules/LabeledSelect.vue";
 
+interface EmailType {
+  id: number;
+  emails: string;
+  domain: string;
+  created_at: Date;
+  checked: boolean;
+}
+
 interface ParamsProps {
   type: string;
   orderByColumn?: string | null;
   orderBy?: string | null;
   offset: number;
   pagination: number;
+  domain?: string;
 }
 
 interface ParamsPropsTypeOnly {
@@ -79,16 +109,22 @@ type ParamsType = ParamsProps | ParamsPropsTypeOnly;
 })
 export default class Table extends Vue {
   private domains: OptionType[] = [];
-  private emailRecords = [];
+  private emailRecords: EmailType[] = [];
   private pagination = 10;
   private sEmail = 0;
   private sTime = 0;
+  private sID = 0;
   private params: ParamsProps = {
     type: "records",
+    orderByColumn: "id",
+    orderBy: "asc",
     offset: 0,
     pagination: this.pagination,
   };
   private sortSymbol = "";
+  private checkedEmails: number[] = [];
+  private allCheck = false;
+  private checks = 0;
 
   async mounted(): Promise<void> {
     const params: ParamsPropsTypeOnly = {
@@ -108,20 +144,55 @@ export default class Table extends Vue {
       if (Object.prototype.hasOwnProperty.call(data, "records")) {
         if (data.records.length) this.emailRecords = data.records;
         else {
-          this.params.offset
-            ? (this.params.offset -= 2 * this.pagination)
-            : null;
+          this.params.offset ? (this.params.offset -= this.pagination) : null;
         }
       } else if (Object.prototype.hasOwnProperty.call(data, "domains")) {
         if (data.domains.length) {
           for (const domain of data.domains) {
             this.domains.push({
               value: domain,
-              label: domain.charAt(0).toUpperCase() + domain.slice(1),
+              label:
+                domain.slice(0, 1) +
+                domain.charAt(1).toUpperCase() +
+                domain.slice(2),
             });
           }
         }
       }
+      this.assignChecks();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async deleteRecord(id: number): Promise<void> {
+    try {
+      await this.axios.delete(`${serverURL}/emails`, { params: { id } });
+      this.fetchEmails(this.params);
+      if (this.emailRecords) this.fetchEmails(this.params);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async exportEmails(): Promise<void> {
+    try {
+      let fData = new FormData();
+      fData.append("email_ids", JSON.stringify(this.checkedEmails));
+      const { data: resultCSV } = await this.axios.post(
+        `${serverURL}/emails`,
+        fData
+      );
+      let blob = new Blob([resultCSV], { type: "application/csv" });
+      let link = document.createElement("a");
+      link.href = window.URL.createObjectURL(blob);
+      link.download = "email_export.csv";
+      link.click();
+      link.remove();
+
+      this.checkedEmails = [];
+      this.emailRecords.map((email) => (email.checked = false));
+      this.allCheck = false;
     } catch (error) {
       console.log(error);
     }
@@ -147,10 +218,38 @@ export default class Table extends Vue {
     }
   }
 
+  public sortIDs(): void {
+    this.params.orderByColumn = "id";
+    this.params.offset = 0;
+    this.sEmail = 0;
+    this.sTime = 0;
+
+    this.setSortSymbol(this.sID);
+    switch (this.sID) {
+      case 0:
+        this.params.orderBy = "desc";
+        ++this.sID;
+        break;
+
+      case 1:
+        this.params.orderBy = "asc";
+        ++this.sID;
+        break;
+
+      case 2:
+        this.params.orderByColumn = "id";
+        this.params.orderBy = "asc";
+        this.sID = 0;
+        break;
+    }
+    this.fetchEmails(this.params);
+  }
+
   public sortEmails(): void {
     this.params.orderByColumn = "email";
     this.params.offset = 0;
     this.sTime = 0;
+    this.sID = 0;
 
     this.setSortSymbol(this.sEmail);
     switch (this.sEmail) {
@@ -165,8 +264,8 @@ export default class Table extends Vue {
         break;
 
       case 2:
-        this.params.orderByColumn = null;
-        this.params.orderBy = null;
+        this.params.orderByColumn = "id";
+        this.params.orderBy = "asc";
         this.sEmail = 0;
         break;
     }
@@ -177,6 +276,7 @@ export default class Table extends Vue {
     this.params.orderByColumn = "created_at";
     this.params.offset = 0;
     this.sEmail = 0;
+    this.sID = 0;
 
     this.setSortSymbol(this.sTime);
     switch (this.sTime) {
@@ -219,6 +319,61 @@ export default class Table extends Vue {
       : (this.params.offset = this.params.pagination);
     this.fetchEmails(this.params);
   }
+
+  public assignChecks(): void {
+    this.checks = 0;
+    this.checkedEmails.map((emId) => {
+      let record = this.emailRecords.find((em) => em.id === emId);
+      if (record) {
+        record.checked = true;
+        ++this.checks;
+      }
+    });
+    this.determineAllCheck();
+  }
+
+  public filterByDomains(domain: string): void {
+    this.params.offset = 0;
+    this.params.domain = domain;
+    this.fetchEmails(this.params);
+  }
+
+  public registerEmailId(e: Event, id: number | undefined): void {
+    const checked = (e.target as HTMLInputElement).checked;
+    if (!id) {
+      if (checked)
+        this.emailRecords.map((email) => {
+          email.checked = true;
+          this.checkedEmails.push(email.id);
+          ++this.checks;
+        });
+      else {
+        const ids = this.emailRecords.map((email) => email.id);
+        this.checkedEmails = this.checkedEmails.filter((emId) => {
+          return !ids.includes(emId);
+        });
+        this.emailRecords.map((email) => {
+          email.checked = false;
+          --this.checks;
+        });
+      }
+    } else {
+      if (checked) {
+        this.checkedEmails.push(id);
+        ++this.checks;
+      } else {
+        this.checkedEmails = this.checkedEmails.filter((emId) => emId !== id);
+        --this.checks;
+      }
+      this.determineAllCheck();
+    }
+  }
+
+  public determineAllCheck(): void {
+    this.checks === this.emailRecords.length
+      ? (this.allCheck = true)
+      : (this.allCheck = false);
+  }
 }
 </script>
 
@@ -228,8 +383,9 @@ export default class Table extends Vue {
   background-color: white;
 }
 .table-controls {
+  display: flex;
+  justify-content: space-between;
   & > div {
-    display: inline-block;
     &:last-child {
       float: right;
       margin-right: 35px;
@@ -237,7 +393,7 @@ export default class Table extends Vue {
   }
 }
 .container {
-  max-width: 60%;
+  max-width: 80%;
   margin: 0 auto;
   display: grid;
   grid-template-columns: 75% 25%;
@@ -247,6 +403,7 @@ export default class Table extends Vue {
   }
 }
 .wrapper {
+  margin: 40px 0;
   height: 100vh;
   & > * {
     width: 100%;
@@ -261,8 +418,14 @@ thead {
   & tr {
     background-color: #bbe4f9;
     & th {
-      &:not(:last-child):hover {
+      &:not(:last-child):hover,
+      &:not(:first-child):hover {
         cursor: pointer;
+      }
+
+      &:last-child,
+      &:first-child {
+        background-color: white;
       }
     }
   }
